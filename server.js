75aa67f22cee8887a85c2a0c2192480902659fa8
sh-osama-sami <85364511+sh-osama-sami@users.noreply.github.com>
2022-12-17 23:31:34 +0200

    server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
